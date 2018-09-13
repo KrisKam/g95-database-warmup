bbe5ce9192be37f5e1b179d81808ce6e9a070425
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-09-13 13:21:53 -0600

    New commit

--- app.js
+++ app.js
@@ -3,18 +3,32 @@ const app = express();
 const host = process.env.PORT || 3000;
 const queries = require("./queries");
 const cors = require("cors")
+const bodyParser = require("body-parser");
 
 app.use(cors());
 
+app.use(bodyParser.json()); 
+
 app.get("/", (req, res) => {
   queries.allStudents()
-  .then(result => res.json({result: result}))
+    .then(result => res.json({result: result}))
 });
 
-app.get("/:firstName", (req, res) => {
-  console.log(req.params)
-  queries.individualStudent(req.params.firstName)
-  .then(student => res.json({"student": student}))
+app.post("/", (req, res, next) => {
+  queries.createStudent(req.body)
+    .then(data => res.json({data}));
+})
+
+app.delete("/:id", (req, res, next) => {
+  queries.deleting(req.params.id)
+    .then(result => {
+      return res.json({result})
+    })
+})
+
+app.get("/:id", (req, res) => {
+  queries.getStudent(req.params.id)
+    .then(student => res.json({"student": student}))
 })
 
 app.listen(host, () => console.log(`Listening on port ${host}`));
--- queries.js
+++ queries.js
@@ -2,12 +2,33 @@ const dbConnection = require("./knexfile")[process.env.NODE_ENV || "development"
 const knex = require("knex")(dbConnection);
 
 
-const allStudents = () => knex.select().from("students");
-const individualStudent = (id) => knex.select().from("students").where("firstName", id);
+const allStudents = () => {
+  return knex.select()
+    .from("students");
+}
+
+const getStudent = (id) => {
+  return knex.select()
+    .from("students")
+      .where("id", id);
+}
+
+const createStudent = (newData) => {
+  return knex("students")
+    .insert(newData, ["id"]);
+};
+const deleting = (id) => {
+  return knex.select()
+    .from("students")
+      .where("id", id)
+        .delete();
+}
 
 module.exports = {
 
   allStudents,
-  individualStudent
+  getStudent,
+  createStudent,
+  deleting
 
 }
